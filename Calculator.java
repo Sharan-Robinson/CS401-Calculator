b49db89c647ed6743aaec6ed01edd202b3b913ef
Sharan_Robinson <sharanrobinson@Sharans-MacBook-Air.local>
2024-02-07 08:35:54 -0800

    Fatimah's Code to fix confirmation

--- Calculator.java
+++ Calculator.java
@@ -10,11 +10,11 @@ public class Calculator {
 		while (true) {
 			System.out.print("Enter the first number:");
 			float firstNumber;
-			
+
 			try{
 			firstNumber = scanner.nextFloat();
 			}catch(InputMismatchException e){
-				System.out.println("Error: You have entered an invalid Value, Please try again."); 
+				System.out.println("Error: You have entered an invalid value, please try again."); 
 				scanner.nextLine();
 
 				continue; 
@@ -27,7 +27,7 @@ public class Calculator {
 			try {
 			secondNumber = scanner.nextFloat();
 			}catch(InputMismatchException e){
-				System.out.println("Error: You have entered an invalid Value, Please try again."); 
+				System.out.println("Error: You have entered an invalid value, please try again."); 
 				scanner.nextLine();
 
 				continue; 
@@ -35,21 +35,21 @@ public class Calculator {
 
 			System.out.println(""); 
 			
-			System.out.println("Select an operation:"); 
+			System.out.println("Select an operation: "); 
 			
 			System.out.println("1. Addition"); 
 			System.out.println("2. Subtraction"); 
 			System.out.println("3. Multiplication"); 
 			System.out.println("4. Division"); 
 
-			System.out.println("Enter the operation number:"); 
+			System.out.println("Enter the operation number: "); 
 			
 			int operator = 0;
 			
 			try {		
 				operator = scanner.nextInt();
 			}catch(InputMismatchException e){
-				System.out.println("Error: You have entered an invalid Value, Please try again."); 
+				System.out.println("Error: You have entered an invalid value, please try again."); 
 				scanner.nextLine();
 				continue; 
 			}
@@ -75,12 +75,12 @@ public class Calculator {
 				
 				while(secondNumber == 0)
 				{
-						System.out.println("Remember, you can not divid by 0, please enter a new number");
+						System.out.println("Remember, you can not divide by 0, please enter a new number: ");
 					
 					try {
 						secondNumber = scanner.nextFloat();
 						}catch(InputMismatchException e){
-							System.out.println("Error: You have entered an invalid Value, Please try again."); 
+							System.out.println("Error: You have entered an invalid value, please try again."); 
 							scanner.nextLine();
 
 							continue; 
@@ -100,10 +100,6 @@ public class Calculator {
 			numbers[3] = "*";
 			numbers[4] = "/";
 			
-	
-
-
-			
 			System.out.println(""); 
 			System.out.print("Result:");
 			System.out.print(firstNumber);
@@ -114,7 +110,6 @@ public class Calculator {
 			System.out.print(" ");
 			System.out.print("=");
 			System.out.print(" ");
-
 			
 			System.out.print(result);
 			System.out.println(""); 
@@ -122,16 +117,26 @@ public class Calculator {
 			if (scanner.hasNextLine()) {
 	            scanner.nextLine();
 	        }
+			
+			String continueLoop;
+			
+            while (true) {
+                System.out.println("Do you want to perform another calculation? (yes/no):");
+                continueLoop = scanner.nextLine();
 
-			System.out.println("Do you want to perform another calculation? (yes/no):");
-			String continueLoop = scanner.nextLine();
-					
-			if(continueLoop.equalsIgnoreCase("no")) {
-				break;
-			}
+                if (continueLoop.equalsIgnoreCase("no") || continueLoop.equalsIgnoreCase("yes")) {
+                    break;
+                } else {
+                    System.out.println("Invalid input. Please specify 'yes' or 'no'.");
+                }
+            }
 
-		}
+            if (continueLoop.equalsIgnoreCase("no")) {
+                break;
+            }
+        }
 		
 		System.out.println("Goodbye!");
+		scanner.close();
 	}
-}
+} 
